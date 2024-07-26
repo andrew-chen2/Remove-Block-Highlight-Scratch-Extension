7a5cbc92756a9f34e6b303eab0f1c12571002990
Andrew Chen <67898430+andrew-chen2@users.noreply.github.com>
2024-07-26 08:50:33 +0100

    Add files via upload

---
+++ content.js
@@ -0,0 +1,16 @@
+function removeHighlightFilter() {
+  const filterElements = document.querySelectorAll('filter[id^="blocklyStackGlowFilter"]');
+  filterElements.forEach(filterElement => filterElement.remove());
+}
+
+removeHighlightFilter()
+
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.addedNodes.length > 0) {
+        removeHighlightFilter();
+      }
+    });
+  });
+  
+  observer.observe(document.body, { childList: true, subtree: true });
